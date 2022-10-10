f0a7d0ba13065ec63f60d0ac4d6d70ab7827ad2e
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-10 13:33:39 +0300

    update

--- open-weather/src/components/SearchBar.js
+++ open-weather/src/components/SearchBar.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { useGlobalContext } from "../context";
 
 const SearchBar = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  function searchData() {
+    console.log("ndani");
+    setSearchTerm(searchValue.current.value);
+    console.log(searchValue.current.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <section>
-      <form>
-        <input />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Search Country location here</label>
+          <input type="text" ref={searchValue} onChange={searchData} />
+        </div>
       </form>
     </section>
   );
--- open-weather/src/context.js
+++ open-weather/src/context.js
@@ -1,26 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback, useContext } from "react";
 import axios from "axios";
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=${process.env.REACT_APP_OPW_API}`;
-
+const base_url = `https://api.openweathermap.org/data/2.5/weather?q=`;
+const city = "nairobi";
 const UserContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const fetchData = async () => {
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [chosenWeather, setChosenWeather] = useState();
+
+  const fetchData = useCallback(async () => {
+    //fetching from api with axios
     try {
-      const data = await axios.get(url);
-      console.log(data);
+      const data = await axios.get(
+        `${base_url}${city}&appid=${process.env.REACT_APP_OPW_API}`
+      );
+      //hapa ndio imefika i think the issue ni niko na hiyo ovject nataka kuiset kama chosenweather
+      const weather = data.data.weather[0];
+      if (weather) {
+        const newWeather = { ...weather };
+      } else {
+      }
     } catch (error) {
       console.log(error);
     }
-  };
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchData();
   });
-  //fadsfds
-  //just to test the git stuff
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+
+  return (
+    <UserContext.Provider value={{ searchTerm, setSearchTerm }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(UserContext);
 };
 
 export { UserContext, AppProvider };
