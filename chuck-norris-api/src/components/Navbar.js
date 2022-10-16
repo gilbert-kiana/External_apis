e4e4d05f095cc7a981ab4bd34549b8ad1b54381d
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-16 20:59:25 +0300

    update

--- chuck-norris-api/src/components/Navbar.js
+++ chuck-norris-api/src/components/Navbar.js
@@ -16,43 +16,7 @@ const Navbar = () => {
   );
 };
 
-import React, { useState } from 'react';
 
-import { CATEGORIES } from '../../constants/categories';
-
-const CategoriesBar = ({ onSelect }) => {
-    const [activeCategory, setActiveCategory] = useState(null);
-
-    const setCategory = e => {
-        const value = e.target.innerText;
-
-        onSelect(value);
-        setActiveCategory(value);
-    };
-
-    return (
-        <>
-            {CATEGORIES.map(cat => (
-                <div key={cat.name} className="my-4">
-                    <h4 className="font-krona mb-2">{cat.name}</h4>
-                    <ul className="ml-4 space-y-2">
-                        {cat.subCategories.map(subCat => (
-                            <li
-                                key={subCat}
-                                onClick={setCategory}
-                                className={`cursor-pointer hover:text-yellow ${
-                                    activeCategory === subCat ? 'text-yellow font-medium' : ''
-                                }`}
-                            >
-                                {subCat}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
-        </>
-    );
-};
 
 export default CategoriesBar;
 
